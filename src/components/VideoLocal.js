f6821f2a60583e478e356efffa67b48522ec11f5
iy06 <yuuki9916@gmail.com>
2021-06-07 19:53:00 +0900

    自身と相手の画面を作成

--- src/components/VideoLocal.js
+++ src/components/VideoLocal.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from 'react';
+import Video from './Video';
 
-const VideoLocal = () => {
+const VideoLocal = ({ name }) => {
   // useRefを初期化
-  const VideoRef = useRef(null);
-  const currentVideoRef = VideoRef.current;
+  const videoRef = useRef(null);
+  const currentVideoRef = videoRef.current;
 
   useEffect(() => {
     if (currentVideoRef === null) return;
@@ -11,9 +12,11 @@ const VideoLocal = () => {
     const getMedia = async () => {
       // 使用するメディアの種類を定義
       const constraints = { audio: true, video: true };
+      console.log("getMedia!")
 
       try {
         const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+        console.log(mediaStream);
         currentVideoRef.srcObject = mediaStream;
       } catch(error) {
         console.log(error)
@@ -21,10 +24,41 @@ const VideoLocal = () => {
     };
 
     getMedia();
+  }, [currentVideoRef]);
 
-  });
-
-  return <></>;
+  return <Video isLocal={true} videoRef={videoRef} name={name}/>;
 };
 
 export default VideoLocal;
+
+// import React, { useEffect, useRef } from 'react';
+
+// import Video from './Video';
+
+// const VideoLocal = ({ name }) => {
+//   const videoRef = useRef(null);
+//   const currentVideoRef = videoRef.current;
+
+//   useEffect(() => {
+//     if (currentVideoRef === null) return;
+
+//     const getMedia = async () => {
+//       const constraints = { audio: true, video: true };
+
+//       try {
+//         const mediaStream = await navigator.mediaDevices.getUserMedia(
+//           constraints
+//         );
+//         currentVideoRef.srcObject = mediaStream;
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+
+//     getMedia();
+//   }, [currentVideoRef]);
+
+//   return <Video isLocal={true} name={name} videoRef={videoRef} />;
+// };
+
+// export default VideoLocal;
--- src/components/VideoRemote.js
+++ src/components/VideoRemote.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Video from './Video';
 
-const VideoRemote = () => {
-  return <></>;
+const VideoRemote = ({ name }) => {
+  // useRefを初期化
+  const videoRef = null;
+
+  return <Video isLocal={false} vidoRef={videoRef} name={name}/>;
 };
 
 export default VideoRemote;
