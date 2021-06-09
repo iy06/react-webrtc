748300fdae47b2fea3adfc29c58af3f48697c7de
iy06 <yuuki9916@gmail.com>
2021-06-09 20:21:19 +0900

    RTCPeerConnectionのインスタンスを作成し、stunサーバーのURLを設定する

---
+++ src/utils/RtcClient.js
@@ -0,0 +1,14 @@
+export default class RtcClient {
+  constructor() {
+    // stunサーバーのurlを設定
+    const config = {
+      iceServers: [{
+          urls: 'stun:stun.stunprotocol.org',
+        },
+      ],
+    }
+    this.rtcPeerConnection = new RTCPeerConnection(config);
+    this.localPeerName = '';
+    this.remotePeerName = '';
+  }
+}
